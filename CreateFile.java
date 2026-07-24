410a2e01d5a9251573ba593bca62f70bdc236f41
safia <safia89khan43@gmail.com>
2026-07-24 13:28:59 +0500

    Add files via upload

--- CreateFile.java
+++ CreateFile.java
@@ -1,19 +1,36 @@
-import java.io.File;      
-import java.io.IOException; 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;   // Import the FileWriter class
 
+
+  
 public class CreateFile {
   public static void main(String[] args) {
     try {
-      File myObj = new File("filename.txt"); 
+      File myObj = new File("helloJava.txt"); 
       if (myObj.createNewFile()) {           
         System.out.println("File created: " + myObj.getName());
       } else {
         System.out.println("File already exists.");
       }
     } catch (IOException e) {
+      System.out.println("An error occurred: " + e.getMessage());
+    }
+  }
+  
+}
+          class WriteToFile {
+  public static void main(String[] args) {  
+  try {
+      FileWriter myWriter = new FileWriter("helloJava.txt");
+      myWriter.write("Files in Java might be tricky, but it is fun enough!");
+      myWriter.close();  // must close manually
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
       System.out.println("An error occurred.");
-      e.printStackTrace(); 
-      
+      e.printStackTrace();
     }
   }
 }
+
+
